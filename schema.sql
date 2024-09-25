26f9ff78f2893c2eb59c48401ef415f2a0d44085
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-25 18:19:17 +0300

    schema.sql

--- schema.sql
+++ schema.sql
@@ -5,3 +5,12 @@ CREATE TABLE "ingredients" (
     "unit" TEXT NOT NULL,
     PRIMARY KEY ("id")
 );
+
+CREATE TABLE "donuts" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    "gluten_free" TEXT NOT NULL,
+    "price_per_donut" REAL NOT NULL,
+    PRIMARY KEY ("id")
+
+);
