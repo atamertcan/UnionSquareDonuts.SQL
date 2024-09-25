47b1b9e5717347fd9523d82ea0696b9a6a305d31
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-25 18:20:43 +0300

    schema.sql

--- schema.sql
+++ schema.sql
@@ -11,6 +11,15 @@ CREATE TABLE "ingredients" (
     PRIMARY KEY ("id")
 );
 
+-- Donuts
+-- We’ll need to include our selection of donuts, past and present!
+-- For each donut on the menu, we’d love to include three things:
+--     * The name of the donut
+--     * Whether the donut is gluten-free
+--     * The price per donut
+-- Oh, and it’s important that we be able to look up the ingredients for each of the donuts!
+
+
 CREATE TABLE "donuts" (
     "id" INTEGER,
     "name" TEXT NOT NULL,
